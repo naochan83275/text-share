b531f4156efbd09d628be23bd9c02fa149f63e99
なおちゃん <166379976+naochan83275@users.noreply.github.com>
2024-08-24 13:00:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 
-var base_url = 'https://naochan83275.github.io/text-share/?'
-if(window.location.href = window.location.href){
+var base_url = 'https://naochan83275.github.io/text-share/?';
+if('window.location.href' = 'window.location.href'){
   if(urlParams.get('mode') = 'HTML'){
       var text = urlParams.get('text');
       if ( ~text.indexOf('</html>')) {
