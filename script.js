3b2650a2050ac89db9b714ce536898bfce1ca464
なおちゃん <166379976+naochan83275@users.noreply.github.com>
2024-08-28 11:33:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const urlParams = new URLSearchParams(window.location.search);
+function _urlparam(paramid){
+  const urlParams = new URLSearchParams(window.location.search);
+  const _param = urlParams.get(paramid);
+  return _param;
+}
 
 var base_url = 'https://naochan83275.github.io/text-share/?';
 if('window.location.href' = 'window.location.href'){
-  if(urlParams.get('mode') = 'HTML'){
-      var text = urlParams.get('text');
+  if(_urlparam('mode') = 'HTML'){
+      var text = _urlparam('text');
       if ( ~text.indexOf('</html>')) {
           var text = text.replace('<!DOCTYPE html>', '');
           var text = text.replace('<html>', '');
